c4b0090a6e8f4b59c72145c05a287ef55e4783eb
Ryan Cahela <ryancahela@gmail.com>
2019-01-29 22:28:08 -0800

    added a try catch block

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 function createGreeting(name,age){
+    const yearOfBirth = getYearOfBirth(age);
+
 
     return `Hi, my name is ${name} and I'm ${age}`;
 }
 
-createGreeting();
+function getYearOfBirth(age) {
 
-const greeting1 = createGreeting();
+    if(age < 0) {
+        throw new Error('Age can not be negative');
+    }
+
+    return 2019 - age;
+}
+
+try {
+    const greeting1 = createGreeting("Ryan", 28);
+    console.log(greeting1);
+} catch(e) {
+    console.log(e.message);
+}
+
+
+createGreeting("Ryan",28);
+
 
 console.log(greeting1);
 
